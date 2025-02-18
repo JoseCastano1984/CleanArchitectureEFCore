76e4bbdb566ca3aede4fda34e483067311872e3d
Jose <jose.castano@sofasandstuff.com>
2025-02-18 23:49:43 +0000

    Initial commit

---
+++ Infrastructure/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+        services.AddDbContext<AppDbContext>(options =>
+        {
+            options.UseSqlServer(
+                "Server=DESKTOP-ROTOGEN;Database=CleanArchitectureEFCoreDb;Trusted_Connection=True;TrustServerCertificate=true;MultipleActiveResultSets=true");
+        });
+        
+        return services;
+    }
+}
