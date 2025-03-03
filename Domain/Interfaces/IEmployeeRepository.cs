ec907021b0330c987d4dd8663dae66b56c81b552
Jose <jose.castano@sofasandstuff.com>
2025-03-03 23:10:12 +0000

    employee repository finished

--- Domain/Interfaces/IEmployeeRepository.cs
+++ Domain/Interfaces/IEmployeeRepository.cs
@@ -5,4 +5,8 @@ namespace Domain.Interfaces;
 public interface IEmployeeRepository
 {
     Task<IEnumerable<Employee>> GetEmployeesAsync();
+    Task<Employee> GetEmployeeByIdAsync(Guid id);
+    Task<Employee> CreateEmployeeAsync(Employee employee);
+    Task<Employee> UpdateEmployeeAsync(Guid employeeId, Employee employee);
+    Task<bool> DeleteEmployeeAsync(Guid employeeId);
 }
--- Infrastructure/Repositories/EmployeeRepository.cs
+++ Infrastructure/Repositories/EmployeeRepository.cs
@@ -19,8 +19,37 @@ public class EmployeeRepository (AppDbContext dbContext) : IEmployeeRepository
 
     public async Task<Employee> CreateEmployeeAsync(Employee employee)
     {
+        employee.Id = Guid.NewGuid();
         await dbContext.Employees.AddAsync(employee);
         await dbContext.SaveChangesAsync();
         return employee;
+    }
+
+    public async Task<Employee> UpdateEmployeeAsync(Guid employeeId, Employee employee)
+    {
+        var employeeRecord = dbContext.Employees.FirstOrDefault(x => x.Id == employeeId);
+        if (employeeRecord is not null)
+        {
+            employeeRecord.FirstName = employee.FirstName;
+            employeeRecord.LastName = employee.LastName;
+            employeeRecord.Email = employee.Email;
+            employeeRecord.PhoneNumber = employee.PhoneNumber;
+            await dbContext.SaveChangesAsync();
+            return employeeRecord;
+        }
+
+        return employee;    
+    }
+
+    public async Task<bool> DeleteEmployeeAsync(Guid employeeId)
+    {
+        var employee = await dbContext.Employees.FirstOrDefaultAsync(x => x.Id == employeeId);
+        if (employee is not null)
+        {
+            dbContext.Employees.Remove(employee);
+            return await dbContext.SaveChangesAsync() > 0;
+        }
+        return false;
+        
     }
 }
