436196669e42ddaf83866b871256cfe3222ec872
Jose <jose.castano@sofasandstuff.com>
2025-03-03 22:14:43 +0000

    forking repository from my personal github

---
+++ Infrastructure/Repositories/EmployeeRepository.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories;
+
+public class EmployeeRepository (AppDbContext dbContext) : IEmployeeRepository
+{
+    public async Task<IEnumerable<Employee>> GetEmployeesAsync()
+    {
+        return await dbContext.Employees.ToListAsync();
+    }
+
+    public async Task<Employee> GetEmployeeByIdAsync(Guid id)
+    {
+        return await dbContext.Employees.FirstOrDefaultAsync(x => x.Id == id);
+    }
+
+    public async Task<Employee> CreateEmployeeAsync(Employee employee)
+    {
+        await dbContext.Employees.AddAsync(employee);
+        await dbContext.SaveChangesAsync();
+        return employee;
+    }
+}
