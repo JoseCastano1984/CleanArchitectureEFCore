3419f65a0c44216631a1e008c3aa42cb375db6d7
JoseCastano1984 <j.albertocast@gmail.com>
2025-04-16 15:28:49 +0100

    changes rider

--- Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
+++ Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76e4bbdb566ca3aede4fda34e483067311872e3d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
